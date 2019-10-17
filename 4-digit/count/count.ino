a29191d2a03ed0c2b75551b1cd2c536942bd2a3f
Aaron Hanson <aaron@acer.cloverfield.net>
2019-10-17 14:26:21 -0700

    successful test fixes

--- 4-digit/count/count.ino
+++ 4-digit/count/count.ino
@@ -1,15 +1,17 @@
 #include <TimerOne.h>
 #include <SPI.h>
 
-int test_value = 0;
+int test_value = 90;
 
 #define DISPLAY_LATCH_PIN 9
+#define RUN_PIN 6
 
 // the setup routine runs once when you press reset:
 void setup() {
+
   pinMode(DISPLAY_LATCH_PIN, OUTPUT);
   Timer1.initialize(5000);
-  Timer1.attachInterrupt(displayService); // runs every 5 msec
+  Timer1.attachInterrupt(display_service); // runs every 5 msec
   SPI.begin();
 }
 
@@ -23,15 +25,15 @@ void loop() {
   */
 
   if (test_value == 101) {
-    update(10000, 1);
+    display_update(10000, 3);
   } else {
-    update(test_value, 1);
+    display_update(test_value, 3);
   }
-  
+
   test_value = test_value + 1;
   if (test_value == 102) {
-    test_value = 0;
+    test_value = 90;
   }
-  
+
   delay(1000);   // wait for a second
 }
--- 4-digit/count/display.ino
+++ 4-digit/count/display.ino
@@ -4,14 +4,16 @@ byte current_dp = 0;
 byte current_on = 0;
 
 /*
-
   left to right digit select map:
   02 16 08 04
-
 */
-byte msel[] = {
-  4, 8, 16, 2
-};
+byte msel[] = {4, 8, 16, 2};
+
+/*
+   decimal significance of each place.
+   for this limited range, a lookup is faster than using exponentiation math (pow)
+*/
+word psig[] = {1, 10, 100, 1000};
 
 /*
   map of LED segments to bit position
@@ -71,24 +73,35 @@ void dec_out(byte pos, byte val, byte dp) {
 
 }
 
-void update(word val, byte dp) {
-  // the current value to be displayed; the device is capable of expressing 0 to 9999.
-  // 10000 or above is interpreted as 'blank' (no output illuminated).
-  byte place, digit;
+/*
+  Set the current value to be displayed (val); the device is capable of expressing 0 to 9999.
+  10000 or above is interpreted as 'blank' (no output illuminated).
 
-  for (place = 3; place >= 0; place--) {
-    if (val > 9999) {
-      current_digits[place] = 10; //blank
+  dp == the number of digits right of the decimal point.
+*/
+void display_update(word val, byte dp) {
+  byte pp, place, digit;
+
+  if (val > 9999) {
+    current_digits[0] = 10;
+    current_digits[1] = 10;
+    current_digits[2] = 10;
+    current_digits[3] = 10;
+    current_dp = 0;
+    current_on = 0;
+    return;
+  }
+
+  for (pp = 4; pp >= 1; pp--) {
+    place = pp - 1;
+    digit = val / psig[place];
+    val = val % psig[place];
+    if (digit > 0) {
+      current_digits[place] = digit;
+    } else if (place <= dp) {
+      current_digits[place] = 0;
     } else {
-      digit = val / pow(10, place);
-      val = int(val) % int(pow(10, place));
-      if (digit > 0) {
-        current_digits[place] = digit;
-      } else if (place <= dp) {
-        current_digits[place] = 0;
-      } else {
-        current_digits[place] = 10; //blank
-      }
+      current_digits[place] = 10; //blank
     }
   }
 
@@ -101,7 +114,7 @@ void update(word val, byte dp) {
    to strobe the display at a rate that is a comfortable 'frame rate'
    for the human eye.
 */
-void displayService(void)
+void display_service(void)
 {
   if (current_dp == current_on) {
     dec_out(current_on, current_digits[current_on], 1);
